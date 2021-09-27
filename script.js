200c9b32ac3659cabc0efa42b4b48025a78109ac
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-09-27 16:00:35 +0200

    Aktualisierung um Punkte aus Issue #75

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 "use district";
 
 console.log("Hallo ToDoApp!");
+
+document.getElementById("btn").addEventListener("click", meine_funktion);
+function meine_funktion() {
+  alert("Hallo nochmal!");
+}
