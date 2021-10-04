64590545065b1bf5a82bfeab2e133dbf6b7dc8a8
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-04 21:30:53 +0200

    Add Todo to List-update

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ console.log("Hallo ToDoApp!");
 const btn = document.querySelector("#btn");
 
 btn.addEventListener("click", meine_funktion);
+
 function meine_funktion() {
   const neuesToDo = document.querySelector("#textbox");
   const ToDoValue = neuesToDo.value;
 
   const newLi = document.createElement("li");
-  newLi.innerText = neuesToDo;
+  newLi.innerText = ToDoValue;
+  console.log(ToDoValue);
 
-  newToDoInputEl.value = "";
+  neuesToDo.value = ` `;
+
+  const list = document.querySelector("#list");
+  list.appendChild(newLi);
 }
