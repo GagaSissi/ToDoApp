d860dc2924022fa78bf3580f2241641d1e2dbe6a
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-07 16:28:58 +0200

    checkbox insert

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ const btn = document.querySelector("#btn");
 // Bei click auf Button, Funktion ausführen
 btn.addEventListener("click", meine_funktion);
 
-// Test, Einfügen von Daten in localstorage
-localStorage.setItem("Nachname", "MeinNachname");
-document.getElementById("list").innerHTML =
-  window.localStorage.getItem("Nachname");
-
 // Funktion
 function meine_funktion() {
   const neuesToDo = document.querySelector("#textbox");
   //Leerstellen raus nehmen, in kleinbuchstaben umwandeln
   const toDoValue = neuesToDo.value.trim().toLowerCase();
-  // Neues Listenelement erzeugen
+  // Erzeugen eines Listenelements
   const newLi = document.createElement("li");
+  // Anhängen der Liste in den DOM
+  const list = document.querySelector("#list");
+  list.appendChild(newLi);
+  // Mit Attribut Checkbox einfügen
+  const todoCheckboxEl = document.createElement("input");
+  todoCheckboxEl.setAttribute("type", "checkbox");
+  list.appendChild(todoCheckboxEl);
+  // Text in Wert umwandeln, überprüfen mit Consolenausgabe
   newLi.innerText = toDoValue;
   console.log(toDoValue);
   // Wert in Testfeld auf Leer ändern
   neuesToDo.value = "";
-  // Anhängen der Liste in den DOM
-  const list = document.querySelector("#list");
-  list.appendChild(newLi);
 }
