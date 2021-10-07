75d7fb428cee6c9012246a8ad0211aa497ed4444
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-07 11:06:40 +0200

    test1: localstorage

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// Test Leerstellen raus nehmen
-let str = "   Hallo ToDoApp!   ";
-str.trim();
-alert(str);
-
 // Teilaufgabe "Hallo" in die Konsole ausgeben
 console.log("Hallo ToDoApp!");
 
@@ -12,16 +7,23 @@ const btn = document.querySelector("#btn");
 // Bei click auf Button, Funktion ausführen
 btn.addEventListener("click", meine_funktion);
 
+// Test, Einfügen von Daten in localstorage
+localStorage.setItem("Nachname", "MeinNachname");
+document.getElementById("list").innerHTML =
+  window.localStorage.getItem("Nachname");
+
+// Funktion
 function meine_funktion() {
   const neuesToDo = document.querySelector("#textbox");
+  //Leerstellen raus nehmen, in kleinbuchstaben umwandeln
   const toDoValue = neuesToDo.value.trim().toLowerCase();
-
+  // Neues Listenelement erzeugen
   const newLi = document.createElement("li");
   newLi.innerText = toDoValue;
   console.log(toDoValue);
-
+  // Wert in Testfeld auf Leer ändern
   neuesToDo.value = "";
-
+  // Anhängen der Liste in den DOM
   const list = document.querySelector("#list");
   list.appendChild(newLi);
 }
