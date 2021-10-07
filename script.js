fa63a58d59443d85f2b125dfbde222e053ae0cb0
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-07 07:55:08 +0200

    Test remove blanks, trim, optical adjustments

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 "use strict";
 
+// Test Leerstellen raus nehmen
+let str = "   Hallo ToDoApp!   ";
+str.trim();
+alert(str);
+
+// Teilaufgabe "Hallo" in die Konsole ausgeben
 console.log("Hallo ToDoApp!");
 
 const btn = document.querySelector("#btn");
-
+// Bei click auf Button, Funktion ausführen
 btn.addEventListener("click", meine_funktion);
 
 function meine_funktion() {
   const neuesToDo = document.querySelector("#textbox");
-  const ToDoValue = neuesToDo.value;
+  const toDoValue = neuesToDo.value.trim().toLowerCase();
 
   const newLi = document.createElement("li");
-  newLi.innerText = ToDoValue;
-  console.log(ToDoValue);
+  newLi.innerText = toDoValue;
+  console.log(toDoValue);
 
-  neuesToDo.value = ` `;
+  neuesToDo.value = "";
 
   const list = document.querySelector("#list");
   list.appendChild(newLi);
